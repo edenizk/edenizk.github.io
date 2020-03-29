bf9c2e9146234aa8c85e4da561516400e51ef395
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-03-29 13:36:44 +0200

    [App] unused stuffs removed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/img/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Home msg="Welcome to Your Home App"/>
     <Navigation/>
+    <Home msg="Welcome to Your Home App"/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
 import Home from './dev/sections/Home.vue';
 import Navigation from './dev/Navigation.vue';
 
@@ -17,19 +14,8 @@ import './dev/scss/main.scss';
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
     Navigation,
     Home,
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
