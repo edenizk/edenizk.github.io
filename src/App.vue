58a5c3efaabff4b850bd2f76cfdc045360ac5ec7
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-04-21 11:36:50 +0200

    [App] AOS initilazied and imported

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,14 @@
 </template>
 
 <script>
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Navigation from './dev/Navigation.vue';
 import Home from './dev/sections/Home.vue';
 import Underground from './dev/sections/Underground.vue';
-
 import './dev/scss/main.scss';
+
+AOS.init();
 
 export default {
   name: 'App',
