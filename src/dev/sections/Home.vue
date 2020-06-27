8416d2fc5b9080dab2afa70ca03facbf5791e5c6
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-06-27 22:37:35 +0200

    [Sections][Home] Simple RWD added

--- src/dev/sections/Home.vue
+++ src/dev/sections/Home.vue
@@ -8,7 +8,7 @@
       <img class="home__sun" data-aos="fade-up-left" data-aos-duration="1500"
        src="../../assets/svg/sun.svg" alt="sun">
       <img class="home__city" data-aos="fade-left" data-aos-duration="1000"
-       src="../../assets/svg/city4.svg" alt="city">
+       src="../../assets/svg/city4.svg" alt="city" data-aos-offset="-50">
       <Waves/>
       <img class="home__front"
        src="../../assets/svg/front_bank.svg" alt="bench and ground">
