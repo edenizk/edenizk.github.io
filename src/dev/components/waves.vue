58d482f528395f039ba0f528c821cb1474011b5f
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-06-27 15:17:30 +0200

    [Components] [Waves] dynamic waves are created

---
+++ src/dev/components/waves.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="waves" :class="darkWaves ? 'waves--dark' : ''">
+    <div class="waves__sea"></div>
+    <svg
+      class="waves__waves"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28"
+      preserveAspectRatio="none"
+      shape-rendering="auto"
+    >
+      <defs>
+        <path
+          id="gentle-wave"
+          d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+        />
+      </defs>
+      <g class="waves__waveList">
+        <use xlink:href="#gentle-wave" x="48" y="0" />
+        <use xlink:href="#gentle-wave" x="48" y="3" />
+        <use xlink:href="#gentle-wave" x="48" y="5" />
+        <use xlink:href="#gentle-wave" x="48" y="7" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Wave',
+  props: {
+    darkWaves: Boolean,
+  },
+};
+</script>
+
+<style>
+</style>
